9602dee234d69fb73b0d11012c8a93bf0ff03468
35A_formation A35A 755 <35A_formation.A35A@si.cnaf.info>
2018-10-24 14:14:25 +0200

    ok

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-password',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordComponent implements OnInit {
 
+  @Input() pwd: string;
   constructor() { }
 
   ngOnInit() {
