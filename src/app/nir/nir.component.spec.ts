9a46b113f8acf90973216b48f8a9521b7c680ae2
35A_formation A35A 755 <35A_formation.A35A@si.cnaf.info>
2018-10-24 09:21:11 +0200

    ok

---
+++ src/app/nir/nir.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NirComponent } from './nir.component';
+
+describe('NirComponent', () => {
+  let component: NirComponent;
+  let fixture: ComponentFixture<NirComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NirComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NirComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/nir/nir.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nir',
+  templateUrl: './nir.component.html',
+  styleUrls: ['./nir.component.css']
+})
+export class NirComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
