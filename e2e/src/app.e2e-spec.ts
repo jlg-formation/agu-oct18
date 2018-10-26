e532fe6d52aeb9e9233df97472bfd02c45e99d1a
35A_formation A35A 755 <35A_formation.A35A@si.cnaf.info>
2018-10-26 12:41:44 +0200

    e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,5 +11,17 @@ describe('workspace-project App', () => {
   it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to exo!');
+  });
+
+  it('should test password', () => {
+
+    // browser.get('/');
+    const input = element(by.css('app-password input[type=password]'));
+    input.clear();
+    browser.sleep(2000);
+    input.sendKeys('coucou');
+    browser.sleep(2000);
+    const message = element(by.css('app-root .message')).getText();
+    expect(message).toEqual('coucou');
   });
 });
